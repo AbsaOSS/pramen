2b10a161262968a3810d679d3450ac920e11adfc
Ruslan Iushchenko <yruslan@gmail.com>
2022-06-21 08:52:57 +0200

    Clean up the pipeline runner.

--- pramen/pipeline-runner/src/main/scala/za/co/absa/pramen/runner/migrations/model0/DataChunk0.scala
+++
@@ -1,8 +0,0 @@
-package za.co.absa.pramen.runner.migrations.model0
-
-case class DataChunk0(tableName: String,
-                      infoDate: String, /* Use String to workaround serialization issues */
-                      inputRecordCount: Long,
-                      outputRecordCount: Long,
-                      jobStarted: Long,
-                      jobFinished: Long)
