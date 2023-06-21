6c47a79cd81725e49a9b4b54dc4f93474ec8f8e9
Jirka Filip <j.f.ilip@seznam.cz>
2023-06-21 10:02:19 +0200

    Fix compatibility with Scala 2.13

--- pramen/core/src/main/scala/za/co/absa/pramen/core/databricks/PramenPyJobTemplate.scala
+++ pramen/core/src/main/scala/za/co/absa/pramen/core/databricks/PramenPyJobTemplate.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package za.co.absa.pramen.core.databricks
 
 import com.typesafe.config.Config
@@ -31,6 +47,7 @@ class PramenPyJobTemplate(config: Config, pythonClass: String, metastoreConfigLo
       value match {
         case map: Map[_, _] => map
           .filterNot(keyVal => isNull(keyVal._2))
+          .toMap
           .mapValues(replaceVariables)
           .toMap
         case seq: Seq[_] => seq
@@ -45,6 +62,7 @@ class PramenPyJobTemplate(config: Config, pythonClass: String, metastoreConfigLo
     map
       .filterNot(keyVal => isNull(keyVal._2))
       .mapValues(replaceVariables)
+      .toMap
   }
 
 }
