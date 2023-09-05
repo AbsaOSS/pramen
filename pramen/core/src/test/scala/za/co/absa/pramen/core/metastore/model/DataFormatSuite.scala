4b923d2da9c0da33687898ece3d4b7b0baf78924
Ruslan Iushchenko <yruslan@gmail.com>
2023-09-05 10:24:00 +0200

    #221 Add support for 'transient' format in metastore configuration.

--- pramen/core/src/test/scala/za/co/absa/pramen/core/metastore/model/DataFormatSuite.scala
+++ pramen/core/src/test/scala/za/co/absa/pramen/core/metastore/model/DataFormatSuite.scala
@@ -18,8 +18,9 @@ package za.co.absa.pramen.core.metastore.model
 
 import com.typesafe.config.ConfigFactory
 import org.scalatest.wordspec.AnyWordSpec
-import za.co.absa.pramen.api.DataFormat.{Delta, Parquet}
-import za.co.absa.pramen.api.Query
+import za.co.absa.pramen.api.DataFormat.{Delta, Parquet, Raw, Transient}
+import za.co.absa.pramen.api.{CachePolicy, DataFormat, Query}
+import za.co.absa.pramen.core.metastore.model.DataFormatParser.{PATH_KEY, TABLE_KEY}
 
 class DataFormatSuite extends AnyWordSpec {
   "fromConfig()" should {
@@ -67,6 +68,47 @@ class DataFormatSuite extends AnyWordSpec {
       assert(format.asInstanceOf[Delta].recordsPerPartition.contains(200))
     }
 
+    "use 'raw' when specified explicitly" in {
+      val conf = ConfigFactory.parseString(
+        """
+          |format = raw
+          |path = /a/b/c
+          |""".stripMargin)
+
+      val format = DataFormatParser.fromConfig(conf, conf)
+
+      assert(format.name == "raw")
+      assert(format.isInstanceOf[Raw])
+      assert(format.asInstanceOf[Raw].path == "/a/b/c")
+    }
+
+    "use 'transient' when specified explicitly" in {
+      val conf = ConfigFactory.parseString(
+        """
+          |format = transient
+          |""".stripMargin)
+
+      val format = DataFormatParser.fromConfig(conf, conf)
+
+      assert(format.name == "transient")
+      assert(format.isInstanceOf[Transient])
+      assert(format.asInstanceOf[Transient].cachePolicy == CachePolicy.NoCache)
+    }
+
+    "support cache policies for 'transient' format" in {
+      val conf = ConfigFactory.parseString(
+        """
+          |format = transient
+          |cache.policy = cache
+          |""".stripMargin)
+
+      val format = DataFormatParser.fromConfig(conf, conf)
+
+      assert(format.name == "transient")
+      assert(format.isInstanceOf[Transient])
+      assert(format.asInstanceOf[Transient].cachePolicy == CachePolicy.Cache)
+    }
+
     "use default records per partition" in {
       val conf = ConfigFactory.parseString(
         """
@@ -110,6 +152,95 @@ class DataFormatSuite extends AnyWordSpec {
 
       assert(ex.getMessage.contains("Mandatory option missing: path"))
     }
+
+    "throw an exception when path is not specified for 'raw'" in {
+      val conf = ConfigFactory.parseString(
+        """
+          |format = raw
+          |""".stripMargin)
+
+      val ex = intercept[IllegalArgumentException] {
+        DataFormatParser.fromConfig(conf, conf)
+      }
+
+      assert(ex.getMessage.contains("Mandatory option for a metastore table having 'raw' format"))
+    }
   }
 
+  "getCachePolicy" should {
+    "return None when no policy is defined" in {
+      val conf = ConfigFactory.empty()
+
+      val policy = DataFormatParser.getCachePolicy(conf)
+
+      assert(policy.isEmpty)
+    }
+
+    "return the proper object for policy 'cache'" in {
+      val conf = ConfigFactory.parseString("cache.policy = cache")
+
+      val policy = DataFormatParser.getCachePolicy(conf)
+
+      assert(policy.isDefined)
+      assert(policy.get == CachePolicy.Cache)
+    }
+
+    "return the proper object for policy 'no_cache'" in {
+      val conf = ConfigFactory.parseString("cache.policy = no_cache")
+
+      val policy = DataFormatParser.getCachePolicy(conf)
+
+      assert(policy.isDefined)
+      assert(policy.get == CachePolicy.NoCache)
+    }
+
+    "return the proper object for policy 'transient'" in {
+      val conf = ConfigFactory.parseString("cache.policy = persist")
+
+      val policy = DataFormatParser.getCachePolicy(conf)
+
+      assert(policy.isDefined)
+      assert(policy.get == CachePolicy.Persist)
+    }
+
+    "throw wn exception on invalid cache policy string" in {
+      val conf = ConfigFactory.parseString("cache.policy = dummy")
+
+      val ex = intercept[IllegalArgumentException] {
+        DataFormatParser.getCachePolicy(conf)
+      }
+
+      assert(ex.getMessage.contains("Incorrect cache policy: dummy"))
+    }
+  }
+
+  "getQuery" should {
+    "return the proper object for path" in {
+      val conf = ConfigFactory.parseString("path = /a/b/c")
+
+      val query = DataFormatParser.getQuery(conf)
+
+      assert(query.isInstanceOf[Query.Path])
+      assert(query.asInstanceOf[Query.Path].path == "/a/b/c")
+    }
+
+    "return the proper object for table" in {
+      val conf = ConfigFactory.parseString("table = db.table")
+
+      val query = DataFormatParser.getQuery(conf)
+
+      assert(query.isInstanceOf[Query.Table])
+      assert(query.asInstanceOf[Query.Table].dbTable == "db.table")
+    }
+
+    "throw an exception when neither path nor table is specified" in {
+      val conf = ConfigFactory.empty()
+
+      val ex = intercept[IllegalArgumentException] {
+        DataFormatParser.getQuery(conf)
+      }
+
+      assert(ex.getMessage.contains(s"Mandatory option missing: $PATH_KEY or $TABLE_KEY"))
+    }
+  }
 }
