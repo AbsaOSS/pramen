e2cb335f64693e4d968f5dcf19243d34fa18742d
Ruslan Iushchenko <yruslan@gmail.com>
2023-10-04 12:33:00 +0200

    #252 Implement the null metadata manager to use for persistent layers that do not support metadata management [yet].

---
+++ pramen/core/src/main/scala/za/co/absa/pramen/core/metadata/MetadataManagerNull.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.pramen.core.metadata
+
+import java.time.LocalDate
+
+class MetadataManagerNull extends MetadataManagerBase(true) {
+  private val errorMessage = "The current bookkeeping persistent layer does not support metadata management."
+
+  def getMetadataFromStorage(tableName: String, infoDate: LocalDate, key: String): Option[String] = {
+    throw new UnsupportedOperationException(errorMessage)
+  }
+
+  def getMetadataFromStorage(tableName: String, infoDate: LocalDate): Map[String, String] = {
+    throw new UnsupportedOperationException(errorMessage)
+  }
+
+  def setMetadataFromStorage(tableName: String, infoDate: LocalDate, key: String, value: String): Unit = {
+    throw new UnsupportedOperationException(errorMessage)
+  }
+
+  def deleteMetadataFromStorage(tableName: String, infoDate: LocalDate, key: String): Unit = {
+    throw new UnsupportedOperationException(errorMessage)
+  }
+
+  def deleteMetadataFromStorage(tableName: String, infoDate: LocalDate): Unit = {
+    throw new UnsupportedOperationException(errorMessage)
+  }
+}
---
+++ pramen/core/src/test/scala/za/co/absa/pramen/core/metadata/MetadataManagerNullSuite.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.pramen.core.metadata
+
+import org.scalatest.wordspec.AnyWordSpec
+
+class MetadataManagerNullSuite extends AnyWordSpec {
+  "getMetadataFromStorage" should {
+    "throw an exception when querying for a key" in {
+      val metadata = new MetadataManagerNull()
+      assertThrows[UnsupportedOperationException](metadata.getMetadataFromStorage(null, null, null))
+    }
+
+    "throw an exception when querying for a map" in {
+      val metadata = new MetadataManagerNull()
+      assertThrows[UnsupportedOperationException](metadata.getMetadataFromStorage(null, null))
+    }
+  }
+
+  "setMetadataFromStorage" should {
+    "throw an exception" in {
+      val metadata = new MetadataManagerNull()
+      assertThrows[UnsupportedOperationException](metadata.setMetadataFromStorage(null, null, null, null))
+    }
+  }
+
+  "deleteMetadataFromStorage" should {
+    "throw an exception when deleting a key" in {
+      val metadata = new MetadataManagerNull()
+      assertThrows[UnsupportedOperationException](metadata.deleteMetadataFromStorage(null, null, null))
+    }
+
+    "throw an exception when deleting all metadata for a table and day" in {
+      val metadata = new MetadataManagerNull()
+      assertThrows[UnsupportedOperationException](metadata.deleteMetadataFromStorage(null, null))
+    }
+  }
+}
