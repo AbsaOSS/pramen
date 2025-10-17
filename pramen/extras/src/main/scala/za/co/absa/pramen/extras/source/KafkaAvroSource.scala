afae3fc365ac3bf78949128956739184aab22199
Ruslan Iushchenko <yruslan@gmail.com>
2025-10-17 09:29:09 +0200

    #644 Add support for arbitrary properties for Kafka connectors.

--- pramen/extras/src/main/scala/za/co/absa/pramen/extras/source/KafkaAvroSource.scala
+++ pramen/extras/src/main/scala/za/co/absa/pramen/extras/source/KafkaAvroSource.scala
@@ -24,13 +24,84 @@ import za.co.absa.abris.config.AbrisConfig
 import za.co.absa.pramen.api.offset.OffsetValue.{KAFKA_OFFSET_FIELD, KAFKA_PARTITION_FIELD, KafkaValue}
 import za.co.absa.pramen.api.offset.{OffsetInfo, OffsetType, OffsetValue}
 import za.co.absa.pramen.api.{ExternalChannelFactoryV2, Query, Source, SourceResult}
+import za.co.absa.pramen.extras.source.KafkaAvroSource.KAFKA_TOKENS_TO_REDACT
+import za.co.absa.pramen.extras.utils.ConfigUtils
 import za.co.absa.pramen.extras.writer.model.KafkaAvroConfig
 
 import java.time.LocalDate
 
+/**
+  * A data source implementation for consuming data from Kafka topics using Avro serialization using Abris
+  * and Confluent Schema Registry integration.
+  *
+  * This source leverages Spark's Kafka integration for batch reading to read messages from a Kafka topic and deserialize them
+  * using the schema definitions from a Schema Registry. It is designed to work only in the incremental mode,
+  * with offset management for as close to the exactly once processing as possible. Exactly once processing is possible
+  * if the Kafka producer can guarantee that duplicate messages are not sent.
+  *
+  * Example source definition:
+  * {{{
+  *  {
+  *    # Define a name to reference from the pipeline:
+  *    name = "kafka_avro"
+  *    factory.class = "za.co.absa.pramen.extras.source.KafkaAvroSource"
+  *
+  *    kafka {
+  *      bootstrap.servers = "mybroker1:9092,mybroker2:9092"
+  *
+  *      # Arbitrary options for creating a Kafka Producer
+  *      # sasl.jaas.config = "..."
+  *      # sasl.mechanism = "..."
+  *      # security.protocol = "..."
+  *      # ...
+  *    }
+  *
+  *    schema.registry {
+  *      url = "https://my.schema.regictry:8081"
+  *
+  *      # Can be one of: topic.name, record.name, topic.record.name
+  *      value.naming.strategy = "topic.name"
+  *
+  *      # If you want to force the specific schema id. Otherwise, the latest schema id will be used.
+  *      # key.schema.id =
+  *      # value.schema.id =
+  *
+  *      # Arbitrary options for the schema registry
+  *      #  basic.auth.credentials.source = "..."
+  *      #  basic.auth.user.info = "..."
+  *      # ...
+  *    }
+  *  }
+  * }}}
+  *
+  * Here is an example of a source definition in a pipeline.
+  *
+  * {{{
+  *  {
+  *    name = "Sourcing from a Kafka topic"
+  *    type = "ingestion"
+  *
+  *    source = "kafka_avro"
+  *
+  *    schedule.type = "incremental"
+  *
+  *    tables = [
+  *      {
+  *        input.table = "my.topic"
+  *        output.metastore.table = "my_table1"
+  *      }
+  *    ]
+  *  }
+  * }}}
+  *
+  * @param sourceConfig    The configuration block that defines the source (it is needed to fulfill the interface).
+  * @param workflowConfig  The configuration of the whole workflow.
+  * @param kafkaAvroConfig The Kafka connection configuration with Schema Registry and Avro options.
+  * @param spark           A Spark session.
+  */
 class KafkaAvroSource(sourceConfig: Config,
                       workflowConfig: Config,
-                      val kafkaConfig: KafkaAvroConfig)
+                      val kafkaAvroConfig: KafkaAvroConfig)
                      (implicit spark: SparkSession) extends Source {
   override def hasInfoDateColumn(query: Query): Boolean = false
 
@@ -86,18 +157,23 @@ class KafkaAvroSource(sourceConfig: Config,
         Map("endingOffsets" -> "latest")
     }
 
+    ConfigUtils.logExtraOptions("Options passed to the Kafka reader:",
+      kafkaAvroConfig.extraOptions,
+      KAFKA_TOKENS_TO_REDACT
+    )
+
     val dfRaw = spark.read
       .format("kafka")
-      .options(kafkaConfig.extraOptions)
-      .option("kafka.bootstrap.servers", kafkaConfig.brokers)
+      .options(kafkaAvroConfig.extraOptions)
+      .option("kafka.bootstrap.servers", kafkaAvroConfig.brokers)
       .option("subscribe", topic)
       .options(startingOffsets)
       .options(endingOffsets)
       .load()
 
     val schemaRegistryClientConfig = Map(
-      AbrisConfig.SCHEMA_REGISTRY_URL -> kafkaConfig.schemaRegistryUrl
-    ) ++ kafkaConfig.schemaRegistryExtraOptions
+      AbrisConfig.SCHEMA_REGISTRY_URL -> kafkaAvroConfig.schemaRegistryUrl
+    ) ++ kafkaAvroConfig.schemaRegistryExtraOptions
 
     // ToDo Add support for other naming strategy and for key deserialization
     val abrisConfig = AbrisConfig
@@ -126,6 +202,8 @@ class KafkaAvroSource(sourceConfig: Config,
 
 object KafkaAvroSource extends ExternalChannelFactoryV2[KafkaAvroSource] {
   val TOPIC_NAME_KEY = "topic.name"
+
+  val KAFKA_TOKENS_TO_REDACT = Set("password", "jaas.config", "auth.user.info")
 
   override def apply(conf: Config, workflowConfig: Config, parentPath: String, spark: SparkSession): KafkaAvroSource = {
     val kafkaReaderConfig = KafkaAvroConfig.fromConfig(conf)
